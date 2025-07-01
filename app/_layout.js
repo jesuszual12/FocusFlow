5181db7d68e3d037a1e5cb4ff62fb655b20112a8
Jesus <josedejesuszual2004@gmail.com>
2025-06-30 19:19:50 -0600

    [fix]: arriglado las exportaciones de la app

---
+++ app/_layout.js
@@ -0,0 +1,10 @@
+import { Stack } from 'expo-router';
+import { View } from 'react-native';
+
+export default function RootLayout() {
+  return (
+    <View style={{ flex: 1 }}>
+      <Stack />
+    </View>
+  );
+}
